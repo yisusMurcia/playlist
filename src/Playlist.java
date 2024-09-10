e8b835f9fd67e3c7e2716cfca2ec1408fda0862f
Yisus <yisusMurci@.com>
2024-09-09 19:11:45 -0500

    Primer avance

---
+++ src/Playlist.java
@@ -0,0 +1,62 @@
+import java.util.ArrayList;
+
+public class Playlist {
+    private String name;
+    private ArrayList<Song> playlistSongs;
+
+    public Playlist(String name) {
+        this.name = name;
+        playlistSongs = new ArrayList<Song>();
+    }
+    public Playlist(){
+        this.name = "My playlist";
+        playlistSongs = new ArrayList<Song>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void displaySongs(){
+        int numOfSongs = playlistSongs.size();
+        System.out.println(name + "has " + numOfSongs + "Songs:");
+        for (Song song : playlistSongs){
+            song.display();
+        }
+
+        System.out.println("\t\t------");
+    }
+
+    public boolean addSong(Song song){
+        //The song can´t be in the playlist
+        for(Song playListSong : playlistSongs){
+            if(playListSong.getName().equalsIgnoreCase(song.getName()) && playListSong.getArtist().equalsIgnoreCase(song.getArtist())){
+                System.out.println("The song is in the playlist");
+                return false;
+            }
+        }
+        playlistSongs.add(song);
+        return true;
+    }
+
+    public void removeSong(Song song){
+        if(playlistSongs.remove(song)){
+            System.out.println("The song has been removed");
+        }else{
+            System.out.println("Something goes wrong");
+        }
+    }
+
+    public Song getSong(String name){
+        for (Song playlistSong : playlistSongs){
+            if(playlistSong.getName().equalsIgnoreCase(name)){
+                return playlistSong;
+            }
+        }
+        return null;
+    }
+}
---
+++ src/Song.java
@@ -0,0 +1,58 @@
+public class Song {
+    private String name;
+    private String artist;
+    private String genre;
+    private int year;
+
+    public Song(String name, String artist){
+        this.name = name;
+        this.artist = artist;
+        genre = "unknown";
+    }
+
+    public Song(String name, String artist, String genre, int year) {
+        this.name = name;
+        this.artist = artist;
+        this.genre = genre;
+        this.year = year;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void display(){
+        System.out.println("Song: " + name + "by: " + artist);
+        if(!genre.equalsIgnoreCase("unknown")){
+            System.out.println("\tGenre: " + genre + "\n\tCreated in" + year);
+        }
+    }
+}
